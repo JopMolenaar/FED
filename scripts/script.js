717a266ed41e86ee466239929bca4570dbf8e235
JopMolenaar <112875494+JopMolenaar@users.noreply.github.com>
2022-09-16 12:04:00 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,12 @@
 // JavaScript Document
 console.log("hi");
+
+
+const toggleButton = document.querySelector('nav a:nth-of-type(1)');
+const section = document.querySelector('nav');
+// const toggleButton = document.getElementsByClassName('toggle-button')[0]
+// const section = document.getElementsByClassName('navigatie')[0]
+
+toggleButton.addEventListener('click', () => {
+    section.classList.toggle('active')
+})
